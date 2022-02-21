01cca27f7ff0429ac7b890d0a05bf4370e9601ec
Illensky <alexis.laroche.02240@gmail.com>
2022-02-21 16:50:19 +0100

    Exercice done

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<head>
+    <link rel="stylesheet" href="/assets/css/main.css">
+</head>
+
 <?php
 
 /**
@@ -13,3 +17,34 @@
  * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
  * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
  */
+
+require __DIR__ . '/Classes/Config.php';
+require __DIR__ . '/Classes/DBSingleton.php';
+
+$pdo = DBSingleton::PDO();
+
+/*
+$stm = $pdo->prepare("
+        SELECT * FROM user
+");
+*/
+
+/*
+$stm = $pdo->prepare("
+        SELECT * FROM user ORDER BY id DESC
+");
+*/
+
+$stm = $pdo->prepare("
+        SELECT nom, prenom FROM user
+");
+
+if ($stm->execute()) {
+    foreach ($stm->fetchAll() as $index => $user) {
+        echo "<div class='user'><h3>Utilisateur ".($index+1)." :</h3>";
+        foreach ($user as $key => $value) {
+            echo "<div>".$key." : ".$value."</div>";
+        }
+        echo "</div>";
+    }
+}
